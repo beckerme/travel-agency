889ff76307ecadfed549aa563c641cfe63eeb4ee
Ewerton Becker <ew_mac@hotmail.com>
2024-11-10 23:31:48 +0000

    aaaaAAAAaaaa

--- src/app.js
+++ src/app.js
@@ -1,21 +1,22 @@
-// app.js ou server.js
 const express = require('express');
-const path = require('path');
-
 const app = express();
+const path = require('path');
 
 // Configura o servidor para servir arquivos estáticos da pasta "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Sua rota principal
+// Configuração da rota principal
 app.get('/', (req, res) => {
   res.send('Bem-vindo a Agencia de Viagens!');
 });
 
-// Outras rotas e configurações...
+// Iniciar o servidor apenas se o arquivo for executado diretamente
+if (require.main === module) {
+  const PORT = 3000;
+  app.listen(PORT, () => {
+    console.log(`Servidor está rodando na porta ${PORT}`);
+  });
+}
 
-// Inicialização do servidor
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+// Exporta o app para que ele possa ser utilizado nos testes
+module.exports = app;
