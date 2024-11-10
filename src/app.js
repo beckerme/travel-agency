3ff24e1ba49af667e5b88a861fa9842483dd89ee
Ewerton Becker <ew_mac@hotmail.com>
2024-11-10 15:05:47 +0000

    ci-cd test

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bem-vindo à Agência de Viagens!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor está rodando na porta ${PORT}`);
+});
+
+module.exports = app;
 
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,1 +1,11 @@
+const request = require('supertest');
+const app = require('../src/app');
+
+describe('Testando rota principal', () => {
+  it('Deve responder com "Bem-vindo à Agência de Viagens!"', async () => {
+    const response = await request(app).get('/');
+    expect(response.text).toBe('Bem-vindo à Agência de Viagens!');
+    expect(response.statusCode).toBe(200);
+  });
+});
 
