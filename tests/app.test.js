e4b150a53814c2f3c804df63cd28585b27c021b7
Ewerton Becker <ew_mac@hotmail.com>
2024-11-10 15:43:09 +0000

    test2

--- tests/app.test.js
+++ tests/app.test.js
@@ -4,7 +4,7 @@ const app = require('../src/app');
 describe('Testando rota principal', () => {
   it('Deve responder com "Olá!"', async () => {
     const response = await request(app).get('/');
-    expect(response.text).toBe('Olá!');
+    expect(response.text).toBe("Olá!");
     expect(response.statusCode).toBe(200);
   });
 });
