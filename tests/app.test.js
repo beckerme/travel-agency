54ed73441267334f7387055b4200f05d5f79c410
Ewerton Becker <ew_mac@hotmail.com>
2024-11-10 15:41:52 +0000

    test

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 const app = require('../src/app');
 
 describe('Testando rota principal', () => {
-  it('Deve responder com "Bem-vindo a Agencia de Viagens!"', async () => {
+  it('Deve responder com "Olá!"', async () => {
     const response = await request(app).get('/');
-    expect(response.text).toBe('Bem-vindo a Agencia de Viagens!');
+    expect(response.text).toBe('Olá!');
     expect(response.statusCode).toBe(200);
   });
 });
